5a182e8d92163473d7a79037d4ce0c47cb8ada46
Firose <firumon@github.com>
2019-09-03 15:29:07 +0530

    init

---
+++ src/QMSServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Milestone\QMS;
+
+use Illuminate\Support\ServiceProvider;
+
+class QMSServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
